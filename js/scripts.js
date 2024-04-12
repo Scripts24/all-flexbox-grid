9814938b075cc2e0b00c40323b0ad682582d363a
Patricia Atanasoff <system.informatica365@gmail.com>
2024-04-12 20:09:49 -0300

    footer, AOS, responsive menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 
-// initialized AOS Librery
+//* Initialized AOS 
 AOS.init();
+
+//* Menú
+const menu = document.getElementById('menu')
+const menuBar = document.getElementById('menu-bar')
+
+menuBar.addEventListener('click', function(){
+    menu.classList.toggle('show-menu')
+})
+
 
